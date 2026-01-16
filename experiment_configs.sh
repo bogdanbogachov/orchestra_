f1944344710d9d74089573823a597ce9649a5996
bogdan128 <bogdan@axya.co>
2026-01-16 15:58:05 -0500

    Exp config

--- experiment_configs.sh
+++ experiment_configs.sh
@@ -39,12 +39,12 @@ add_experiments() {
 }
 
 # Format: add_experiments "base_name" "eval_head" "custom" "pool" "fft" "d_inf" "c_inf" global_exp_num per_config_start per_config_end
-#add_experiments "banking77_default" "default_head" "False" "mean" "False" "True" "False" 12 1 3
+add_experiments "clinc150_default" "default_head" "False" "mean" "False" "True" "False" 21 1 1
 #add_experiments "banking77_custom_last" "custom_head" "True" "last" "False" "False" "True" 12 1 3
 #add_experiments "banking77_custom_max" "custom_head" "True" "max" "False" "False" "True" 12 1 3
 #add_experiments "banking77_custom_mean" "custom_head" "True" "mean" "False" "False" "True" 12 1 3
-#add_experiments "banking77_custom_attention" "custom_head" "True" "attention" "False" "False" "True" 12 1 3
+add_experiments "clinc150_custom_attention" "custom_head" "True" "attention" "False" "False" "True" 21 1 1
 #add_experiments "banking77_fft100_custom_fft_last" "custom_head" "True" "last" "True" "False" "True" 20 1 4
 #add_experiments "banking77_fft100_custom_fft_max" "custom_head" "True" "max" "True" "False" "True" 20 1 4
 #add_experiments "banking77_fft100_custom_fft_mean" "custom_head" "True" "mean" "True" "False" "True" 20 1 4
-add_experiments "clinc150_fft50_custom_fft_attention" "custom_head" "True" "attention" "True" "False" "True" 21 1 1
+#add_experiments "clinc150_fft50_custom_fft_attention" "custom_head" "True" "attention" "True" "False" "True" 21 1 1
