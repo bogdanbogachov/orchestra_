44eeeb3a6b3fb61834e1dff90dbf4308e05e1c3a
bogdanbogachov <bogdan@axya.co>
2026-07-27 16:24:01 -0400

    Adjusted additional data to keep 5 classes only

--- experiment_configs.sh
+++ experiment_configs.sh
@@ -39,7 +39,7 @@ add_experiments() {
 }
 
 # Format: add_experiments "base_name" "eval_head" "custom" "pool" "fft" "d_inf" "c_inf" global_exp_num per_config_start per_config_end
-add_experiments "additional_noise_default" "default_head" "False" "mean" "False" "True" "False" 69 1 1
+add_experiments "additional_noise_default" "default_head" "False" "mean" "False" "True" "False" 699 1 1
 #add_experiments "additional_noise_custom_last" "custom_head" "True" "last" "False" "False" "True" 70 1 10
 #add_experiments "additional_noise_custom_max" "custom_head" "True" "max" "False" "False" "True" 70 1 10
 #add_experiments "additional_noise_custom_mean" "custom_head" "True" "mean" "False" "False" "True" 70 1 10
