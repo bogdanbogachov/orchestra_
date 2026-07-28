4ae56b681227d3368e0ba27a43d47f5dbe1b8154
bogdanbogachov <bogdan@axya.co>
2026-07-28 13:50:17 -0400

    Adjusted baseline job names

--- baseline_experiment_configs.sh
+++ baseline_experiment_configs.sh
@@ -23,5 +23,5 @@ add_baseline_experiments() {
 }
 
 # Edit global experiment numbers before submitting if needed.
-add_baseline_experiments "additional_noise_sbert_linear" "sbert_linear" 72 1 10
-add_baseline_experiments "additional_noise_distilbert_cls" "distilbert_cls" 72 1 10
+add_baseline_experiments "real_sbert_linear" "sbert_linear" 72 1 10
+add_baseline_experiments "real_distilbert_cls" "distilbert_cls" 72 1 10
